6f896d3adef4c563f27b587a4458efb4d80fa0bc
seb <contact@smaloron.eu>
2023-01-09 14:10:36 +0100

    app simple avec create-react-app

---
+++ my-app/src/App.js
@@ -0,0 +1,19 @@
+import Card from './components/Card';
+
+const cardList = [
+  "Berlin", "Glasgow", "Kiev", "Fukuoka"
+];
+
+function getCards () {
+  return cardList.map((item) => <Card title={item} />)
+}
+function App () {
+  return (
+    <div className="App">
+      <h1>Hello gals and guys</h1>
+      {getCards()}
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/Card.js
@@ -0,0 +1,9 @@
+function Card (props) {
+    return (
+        <div>
+            <h1>{props.title}</h1>
+        </div>
+    )
+}
+
+export default Card;
